8382dab9994bc9cb570ef873765fc3ec57eaf1a4
kekonline <kekonline@gmail.com>
2023-09-07 12:18:42 +0200

    small loader and minor issue

--- src/services/service.config.js
+++ src/services/service.config.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 //this is the creative service we have done to access axios in a short way manner Plus being able to send as header the token stored in the users local storage plus here we can handle the base URL for when the app is deployed or it's being developed in local host mode
 const service = axios.create({
-    baseURL: "http://localhost:5005/api"
-    // baseURL: import.meta.env.VITE_SERVER_URL,
+    // baseURL: "http://localhost:5005/api"
+    baseURL: import.meta.env.VITE_SERVER_URL,
 });
 
 service.interceptors.request.use((config) => {
